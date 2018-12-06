3879dd0dafe34b5892ece94be854126ea2c856cc
Estudiante PUCESE <heidi.valle@pucese.edu.ec>
2018-12-06 09:24:58 -0500

    Primer Comic      Paso de vectores como parametro de un funcion

--- VectoresParam/Program.cs
+++ VectoresParam/Program.cs
@@ -8,8 +8,41 @@ namespace VectoresParam
 {
     class Program
     {
+        const int MAX = 10;
         static void Main(string[] args)
         {
+            int[] x = new int[MAX];
+            leer(x);
+            imprimir(x);
+            Console.ReadKey();
+
+
+
+
         }
+        public static void leer(int[] x)
+        {
+            for(int i=0; i<MAX; i++)
+            {
+                Console.WriteLine("\nIngresa del elemento {0}", i + 1);
+                x[i] = Int32.Parse(Console.ReadLine());
+
+
+            }
+            
+
+
+        }
+        public static void imprimir(int[] x)
+        {
+            foreach (int num in x)
+            {
+                Console.WriteLine("\nlememto {0}", num);
+            }
+        }
+
+        public static void parametro()
+
     }
+
 }
