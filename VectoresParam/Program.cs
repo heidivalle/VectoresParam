168194d66831248395bca7a6feb8f59f88b859c4
Estudiante PUCESE <heidi.valle@pucese.edu.ec>
2018-12-06 09:58:18 -0500

    Ejercicio en clase 6

--- VectoresParam/Program.cs
+++ VectoresParam/Program.cs
@@ -14,25 +14,34 @@ namespace VectoresParam
             int[] x = new int[MAX];
             leer(x);
             imprimir(x);
+            if (buscar(x, 15))
+                Console.WriteLine("\n15 encontrado");
+            else
+                Console.WriteLine("\n15 NO EXISTE ");
             Console.ReadKey();
-
+            if (buscar(x, 25))
+                Console.WriteLine("\n25 encontrado");
+            else
+                Console.WriteLine("\n25 NO EXISTE ");
+            Console.ReadKey();
 
 
 
         }
         public static void leer(int[] x)
         {
-            for(int i=0; i<MAX; i++)
+            for (int i = 0; i < MAX; i++)
             {
                 Console.WriteLine("\nIngresa del elemento {0}", i + 1);
                 x[i] = Int32.Parse(Console.ReadLine());
 
 
             }
-            
+
 
 
         }
+        //Agregar el metodo que recibe como parametro un entero y devuelve (V) y si estae entero existe en el vector; devuelve (f) si no existe //
         public static void imprimir(int[] x)
         {
             foreach (int num in x)
@@ -41,8 +50,38 @@ namespace VectoresParam
             }
         }
 
-        public static void parametro()
+        public static bool buscar(int[] x, int y)
+        {
+            bool encontrado = false;
+            foreach (int num in x)
+            {
+                if (num == y)
+                {
+                    encontrado = true;
+                    break;
+                }
+            }
+            return encontrado;
+
+        }
+        public static bool buscar2(int[] x, int y)
+        {
+               bool encontrado = false;
+                int i = 0;
+                while (i < MAX && encontrado == false)
+                { 
+                    if (x[i] == y)
+                        encontrado = true;
+                    i++;
+                }
+                return encontrado;                                 
+           
+
+        }
+    
 
     }
-
+   
 }
+
+
